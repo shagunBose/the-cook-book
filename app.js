f1fc6386506a46518b6548bc52cf19b6c7ffad74
Shagun Bose <32908915+shagunBose@users.noreply.github.com>
2018-08-14 16:21:57 +0530

    created barebones structure of the cook book

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+var recipes = [
+  {name: "Chocolate Pancakes", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Penne Arabiata", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Margaritta Pizaa", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Homemade Sponge Cake", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Chocolate Pancakes", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Penne Arabiata", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Margaritta Pizaa", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Homemade Sponge Cake", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Chocolate Pancakes", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Penne Arabiata", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Margaritta Pizaa", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"},
+  {name: "Homemade Sponge Cake", image: "https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png"}
+]
+
+//ROUTES
+app.get('/', function(req, res){
+  res.render("landing");
+})
+
+app.get('/recipes', function(req, res){
+  res.render("recipes", {recipes: recipes} );
+});
+
+app.post('/recipes', function(req, res){
+  //get data from form and add to recipes array
+  var name = req.body.name;
+  var image = 'https://freepngimg.com/download/cooking_tools/7-2-cooking-tools-png-clipart.png';
+  recipes.push({name: name, image: image});
+  //redirect to /recipes
+  res.redirect('/recipes');
+});
+
+app.get('/recipes/new', function(req, res){
+  res.render("newRecipe");
+});
+
+
+//SERVER STUFF//
+app.listen(3000, function(){
+  console.log("The Cook Book Server has started");
+});
